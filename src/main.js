4858734c7c2a1c2ac9247ac01d65c56b59199839
pinkcao <972558143@qq.com>
2021-01-22 11:18:42 +0800

    删除多余包

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 // import Vuex from 'vuex';
 // import App from "./App";
+import Vue from 'vue'
 import testLayout from './layout/testLayout.vue'
 import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import less from 'less'
-import store from './store'
 import VueDragResize from 'vue-drag-resize'
 import VueDragResizeRotate from './common/commonComponents/vueDragResizeRotate/vueDragResizeRotate.vue'
 import url from '@mock/mockAPI.js'
+import store from './store'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
-Vue.prototype.$axios = axios
 Vue.use(VueAxios, axios)
-Vue.config.productionTip = false
 Vue.use(less)
 Vue.component('vue-drag-resize', VueDragResize)
 Vue.component('vue-drag-resize-rotate', VueDragResizeRotate)
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 Vue.prototype.$url = url
 
 // Vue.use(Vuex);
