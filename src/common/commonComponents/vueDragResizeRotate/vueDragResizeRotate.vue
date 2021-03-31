e3e6ae406fde113c6de058b74dcd9f8dfaec54c9
pinkcao <972558143@qq.com>
2021-03-31 11:33:28 +0800

    更新了两点间距离计算公式

--- src/common/commonComponents/vueDragResizeRotate/vueDragResizeRotate.vue
+++ src/common/commonComponents/vueDragResizeRotate/vueDragResizeRotate.vue
@@ -694,7 +694,9 @@ export default {
     },
     //朴素的两点间距离公式
     calculLength(x1, x2, y1, y2) {
-      return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+      // console.log(Math.hypot(x1 - x2, y1 - y2))
+      // console.log(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)))
+      return Math.hypot(x1 - x2, y1 - y2)
     },
     //余弦定理求角度
     calculrawDegA(a, b, c) {
