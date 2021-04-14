ea75fc1061b68abc006a04526cc0ead9aed665e7
pinkcao <972558143@qq.com>
2021-04-14 09:32:23 +0800

    update gitignore in with3D branch

--- src/components/visComponents/mapComponent/mapComponent.vue
+++ src/components/visComponents/mapComponent/mapComponent.vue
@@ -148,13 +148,6 @@ export default {
       loadedDataPercentage: 0,
       loadModelFlag: true,
       progressWidth: 200
-      // progressColor: [
-      //   { color: '#f56c6c', percentage: 20 },
-      //   { color: '#e6a23c', percentage: 40 },
-      //   { color: '#5cb87a', percentage: 60 },
-      //   { color: '#1989fa', percentage: 80 },
-      //   { color: '#6f7ad3', percentage: 100 }
-      // ]
     }
   },
   props: {},
